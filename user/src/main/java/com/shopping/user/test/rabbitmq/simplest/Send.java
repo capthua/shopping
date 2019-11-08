3310553cf508ad2543d494106d14195a29833a71
hansh <hansh@ilinecn.com>
2019-11-08 18:10:56 +0800

    添加mq最简单的发送和接受代码

---
+++ user/src/main/java/com/shopping/user/test/rabbitmq/simplest/Send.java
@@ -0,0 +1,30 @@
+package com.shopping.user.test.rabbitmq.simplest;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) {
+        ConnectionFactory factory=new ConnectionFactory();
+        factory.setHost("localhost");
+        try {
+            Connection connection=factory.newConnection();
+            Channel channel=connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            for(int i=0;i<100;i++){
+                message+=i;
+                Thread.sleep(5000);
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println(" [x] Sent '" + message + "'");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
