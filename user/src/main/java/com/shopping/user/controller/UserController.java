cede736d56741980ee3e55c7067c3b2bc2d8f300
hansh <hansh@ilinecn.com>
2019-11-19 15:21:24 +0800

    添加topic模式收发消息

--- user/src/main/java/com/shopping/user/controller/UserController.java
+++ user/src/main/java/com/shopping/user/controller/UserController.java
@@ -7,8 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 /**
  * describe:
@@ -18,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 //@Slf4j //自动生成log变量
 @RestController
-@RequestMapping("user")
+@RequestMapping("users")
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -30,6 +33,13 @@ public class UserController {
     String testUser(String name) {
         logger.info("testUser参数:{}", name);
         String hehe = userService.testUser(name);
+        return "hehe";
+    }
+
+
+    @GetMapping("/{name}")
+    String testPathVariableWithDot(@PathVariable("name") String name){
+        logger.info(name);
         return "hehe";
     }
 }
