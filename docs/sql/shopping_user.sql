b9b56c7aa00db3073bbbd35d418089d36ef864b0
ffulauh <hshua24@gmail.com>
2020-01-02 23:03:02 +0800

    修改user表字段

--- docs/sql/shopping_user.sql
+++ docs/sql/shopping_user.sql
@@ -1,16 +1,20 @@
-create schema if not exists shopping DEFAULT CHARSET=utf8;
+create schema if not exists shopping DEFAULT CHARSET = utf8;
 
 use shopping;
 
-CREATE TABLE IF NOT EXISTS `user` (
-                                      `id` int(11) NOT NULL AUTO_INCREMENT,
-                                      `username` varchar(24) NOT NULL,
-                                      `password` varchar(24) NOT NULL,
-                                      `fullName` varchar(96) NOT NULL,
-                                      `email` varchar(48) NOT NULL,
-                                      `updateByEmail` tinyint(1) NOT NULL DEFAULT '0',
-                                      PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `user`
+(
+    `id`              int(11)     NOT NULL AUTO_INCREMENT,
+    `username`        varchar(24) NOT NULL,
+    `password`        varchar(24) NOT NULL,
+    `full_name`       varchar(96) NOT NULL,
+    `email`           varchar(48) NOT NULL,
+    `update_by_email` tinyint(1)  NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`)
 );
 
-INSERT INTO user (username, password, fullName, email, updateByEmail) VALUES ('hsh', '02042018', '韩少华', 'hshua24@gmail.com', 0);
-INSERT INTO user (username, password, fullName, email, updateByEmail) VALUES ('steve', '22222', '乔布斯', 'steve@apple.com', 0);
+INSERT INTO dps.user (username, password, full_name, email, update_by_email)
+VALUES ('hsh', '02042018', '韩少华', 'hshua24@gmail.com', 0);
+INSERT INTO dps.user (username, password, full_name, email, update_by_email)
+VALUES ('steve', '22222', '乔布斯', 'steve@apple.com', 0);
+
