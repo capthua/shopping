0035b48dff14ab84b7c4d2b93a83b2db8db04918
hansh <hansh@ilinecn.com>
2020-11-20 17:37:01 +0800

    添加日志配置

--- demo/src/main/java/com/shopping/demo/controller/AController.java
+++ demo/src/main/java/com/shopping/demo/controller/AController.java
@@ -4,14 +4,20 @@ import com.shopping.common.spring.ApplicationContextUtils;
 import com.shopping.demo.service.RedisSampleService;
 import com.shopping.demo.service.RedissonSampleService;
 import io.prs.mybatisx.autoconfigure.MybatisxTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @RequestMapping("a")
 public class AController {
+
+    private static final Logger logger= LoggerFactory.getLogger(AController.class);
 
     @Autowired
     MybatisxTemplate template;
@@ -30,7 +36,21 @@ public class AController {
 //        redisSampleService.setSamples();
 //        redisSampleService.hashSamples();
 //        redisSampleService.zSetSamples();
-        redissonSampleService.redissonSamples();
+//        redissonSampleService.redissonSamples();
+        return template.getProperties().toString();
+    }
+
+    @GetMapping("logtest")
+    public String logtest()throws Exception{
+        logger.debug("debug");
+        logger.info("info");
+
+//        for(int i=0;i<500000;i++){
+//            if(i%10000==0){
+//                TimeUnit.SECONDS.sleep(2);
+//            }
+//            logger.info("info");
+//        }
         return template.getProperties().toString();
     }
 
