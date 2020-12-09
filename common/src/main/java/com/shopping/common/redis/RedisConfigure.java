6e63438e2c355c38d6eecd6928298c074324f680
hansh <hansh@ilinecn.com>
2020-12-09 14:45:45 +0800

    去掉redis-sentinel中的密码

--- common/src/main/java/com/shopping/common/redis/RedisConfigure.java
+++ common/src/main/java/com/shopping/common/redis/RedisConfigure.java
@@ -8,15 +8,15 @@ import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 //@Configuration
-//public class RedisConfigure {
-//
-//    @Bean
-//    public RedisConnectionFactory lettuceConnectionFactory(){
-//        RedisSentinelConfiguration sentinelConfig=new RedisSentinelConfiguration().master(
-//                "mymaster").sentinel("127.0.0.1",26379).sentinel("127.0.0.1",26380).
-//                sentinel("127.0.0.1",26381);
-//        sentinelConfig.setPassword(RedisPassword.of("hanhanhan"));
-//        return new LettuceConnectionFactory();
-//    }
-//
-//}
+public class RedisConfigure {
+
+    @Bean
+    public RedisConnectionFactory lettuceConnectionFactory(){
+        RedisSentinelConfiguration sentinelConfig=new RedisSentinelConfiguration().master(
+                "mymaster").sentinel("127.0.0.1",26379).sentinel("127.0.0.1",26380).
+                sentinel("127.0.0.1",26381);
+        sentinelConfig.setPassword(RedisPassword.of("hanhanhan"));
+        return new LettuceConnectionFactory(sentinelConfig);
+    }
+
+}
