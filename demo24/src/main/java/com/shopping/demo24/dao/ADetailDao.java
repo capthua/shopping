e0bfd6e0fb1e2a1ca26c86ee8da256390daf6ef6
CaptHua <capthua24@gmail.com>
2023-11-07 16:55:55 +0800

    optimize: optimize demo24 module

--- demo24/src/main/java/com/shopping/demo24/dao/ADetailDao.java
+++
@@ -1,7 +0,0 @@
-package com.shopping.demo24.dao;
-
-import com.shopping.common.db.CommonMapper;
-import com.shopping.demo24.model.ADetail;
-
-public interface ADetailDao extends CommonMapper<ADetail> {
-}
